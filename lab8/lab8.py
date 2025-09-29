149b3ade439a42019b05db8288c404cccff78d03
Roman Eidelman <eidelmanrv@gmail.com>
2025-09-29 16:44:17 +0300

    Add lab8

---
+++ lab8/lab8.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+res = 10                 # grid resolution
+map_len = 200                   # map length
+robot_pos = 0.0                 # robot position (at c0)
+measurements = np.array([101, 82, 91, 112, 99, 151, 96, 85, 99, 105], dtype=float)
+
+p_free = 0.3                  # p for cells closer than z
+p_occ  = 0.6                  # p for cells in [z, z+20]
+extra = 20.0                # width of "hit" interval
+prior = 0.5
+
+def logit(p: np.ndarray):
+    return np.log(p / (1.0 - p))
+
+def sigmoid(x: np.ndarray):
+    return 1.0 / (1.0 + np.exp(-x))
+
+c = np.arange(0, map_len + res, res, dtype=float)  # cell coordinates (cm)
+l = np.zeros(c.shape[0], dtype=float)
+
+for z in measurements:
+    d = c - robot_pos
+    p_m = np.full(d.shape[0], 0.5, dtype=float)
+
+    p_m[d < z] = p_free
+
+    start = z
+    end = z + extra
+    mask_hit = (d >= start) & (d <= end)
+    p_m[mask_hit] = p_occ
+
+    l += logit(p_m)
+
+m = sigmoid(l)
+
+plt.figure(figsize=(12, 8))
+plt.plot(c, m)
+plt.xlabel("cells, cm")
+plt.ylabel("p_occ")
+plt.grid(True)
+plt.show()
